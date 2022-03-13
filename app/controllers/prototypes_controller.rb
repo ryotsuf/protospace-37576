934fd59ef795b6163ef7866e5a90170fa8b48898
ryotsuf <mail.ryoto.ino@gmail.com>
2022-03-13 15:32:02 +0900

    editあくしょんの変更カレントユーザーを変えた

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -52,9 +55,9 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless current_user.id == @prototype.user_id
-      redirect_to root_path
+    # @prototype = Prototype.find(params[:id])
+    unless user_signed_in?
+      redirect_to action: :index
     end
   end
 end
